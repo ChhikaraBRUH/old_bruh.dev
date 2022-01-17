eaec24920d3296f879d687565a4ad39c46dbd435
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-01-17 19:48:31 +0530

    add back to top btn

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Works");
+
+let topBtn = document.getElementById("top-btn");
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		topBtn.style.display = "block";
+	} else {
+		topBtn.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
