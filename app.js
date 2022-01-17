dbe8ecccd1e81fa50532298c44c984b5f7fbfd10
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-01-17 20:31:07 +0530

    add back to top btn to all pages

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-console.log("Works");
-
 let topBtn = document.getElementById("top-btn");
 
-window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
+window.onscroll = () => {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		topBtn.style.display = "block";
 	} else {
 		topBtn.style.display = "none";
 	}
-}
+};
 
-function topFunction() {
+let topFunction = () => {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
